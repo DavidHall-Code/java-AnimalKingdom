852896df36246fae56a048fdc84f597ad8dbe49b
DavidHall-Code <david_m_hall@outlook.com>
2021-03-09 17:52:43 -0500

    setup Mammals.java

--- src/animalkingdom/Mammals.java
+++ src/animalkingdom/Mammals.java
@@ -1,6 +1,32 @@
 package animalkingdom;
 
-public class Mammals
+public class Mammals extends Animals
 {
-     
-}
+     public Mammals(String name, int yearDiscovered)
+     {
+          super(name, yearDiscovered);
+     }
+
+     // FROM ANIMALS
+     // public abstract String move();
+     // public abstract String breath();
+     // public abstract String reproduce();
+
+     @Override
+     public String move()
+     {
+          return "Walk";
+     }
+
+     @Override
+     public String breathe()
+     {
+          return "Lungs";
+     }
+
+     @Override
+     public String reproduce()
+     {
+          return "Live Birth";
+     }
+} // Class End
