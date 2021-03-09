040e57e6960829f8f1c9899dc918bbd985e489b5
DavidHall-Code <david_m_hall@outlook.com>
2021-03-09 17:58:43 -0500

    setup Birds.java

--- src/animalkingdom/Birds.java
+++ src/animalkingdom/Birds.java
@@ -1,6 +1,32 @@
 package animalkingdom;
 
-public class Birds
+public class Birds extends Animals
 {
-     
-}
+     public Birds(String name, int yearDiscoverd)
+     {
+          super(name, yearDiscovered);
+     }
+
+     // FROM ANIMALS
+     // public abstract String move();
+     // public abstract String breath();
+     // public abstract String reproduce();
+
+     @Override
+     public String move()
+     {
+          return "Fly";
+     }
+
+     @Override
+     public String breathe()
+     {
+          return "Lungs";
+     }
+
+     @Override
+     public String reproduce()
+     {
+          return "Eggs";
+     }
+} // Class End
