9ebcfc812d9017d93c9f555c0bd606b65ea2e58d
DavidHall-Code <david_m_hall@outlook.com>
2021-03-09 18:03:48 -0500

    setup Fish.java

--- src/animalkingdom/Fish.java
+++ src/animalkingdom/Fish.java
@@ -1,6 +1,32 @@
 package animalkingdom;
 
-public class Fish
+public class Fish extends Animals
 {
-     
-}
+     public Fish(String name, int yearDiscovered)
+     {
+          super(name, yearDiscoverd);
+     }
+
+     // FROM ANIMALS
+     // public abstract String move();
+     // public abstract String breath();
+     // public abstract String reproduce();
+
+     @Override
+     public String move()
+     {
+          return "Swim";
+     }
+
+     @Override
+     public String breathe()
+     {
+          return "Gills";
+     }
+
+     @Override
+     public String reproduce()
+     {
+          return "Eggs";
+     }
+} // Class End
